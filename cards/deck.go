2ff5d21e641e55c980641337053c0d172b1b512d
darthnater007 <nsperk@gmail.com>
2020-02-01 18:02:55 -0500

    multiple returns, slice range, new deck and deal function

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,26 @@ import "fmt"
 // Create a new type, deck: a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// print contents of deck type
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
