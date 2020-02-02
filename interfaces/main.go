035a68516e3f736d6ea932377622fce2042e906a
darthnater007 <nsperk@gmail.com>
2020-02-02 15:43:54 -0500

    begin interface instruction

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	//let's pretend this logic is significantly different from spanish bot for demonstration
+	return "Hello!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
