c17b06ff6613672af50eb7777105f646a4d52e17
darthnater007 <nsperk@gmail.com>
2020-02-02 15:20:46 -0500

    finish map instruction

--- map/main.go
+++ map/main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors := make(map[string]string)
+	printMap(colors)
 
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-	fmt.Println(colors)
+}
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
